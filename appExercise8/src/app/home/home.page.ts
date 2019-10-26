9b29d3b66b28e6f308d0cb3e296b63c5f707488e
CapiHidalgo <alvaro.hidalgo@ununuzi.es>
2019-10-26 18:32:17 +0200

    Page with one button to turn on or turn off the flash. If the device has not Flash this button would not appear

--- appExercise8/src/app/home/home.page.ts
+++ appExercise8/src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
+
+import	{	Platform	}	from	'@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  show = false;
+  on = false;
+  constructor(
+    private flashlight: Flashlight
+  ) {}
 
-  constructor() {}
 
+ async isAvailable(): Promise<boolean> {
+    return await this.flashlight.available();
+  }
+  ngOnInit() {}
+
+  async flash() {
+    this.show = await this.isAvailable();
+    if (this.show) {
+      this.flashlight.toggle();
+    } else {
+      console.log('no flash');
+    }
+  }
 }
